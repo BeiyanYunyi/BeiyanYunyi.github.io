668a2acc9024a9ec33e6e2c46fc60ca38d2bd1d9
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-07-05 13:23:12 +0800

    🎨 Edge case

--- src/utils/darkMode.tsx
+++ src/utils/darkMode.tsx
@@ -1,6 +1,9 @@
 import { createEffect, createSignal } from 'solid-js';
 
 const [dark, setDark] = createSignal(window.matchMedia('(prefers-color-scheme: dark)').matches);
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+  setDark(e.matches);
+});
 createEffect(() => {
   if (dark()) {
     window.document.documentElement.classList.add('dark');
