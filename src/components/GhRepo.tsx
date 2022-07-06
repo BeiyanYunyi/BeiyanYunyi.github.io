3b9d3bf84ae543dc256c1578fa48c5fea74c7f15
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-07-06 21:32:58 +0800

    💄 Styling

--- src/components/GhRepo.tsx
+++ src/components/GhRepo.tsx
@@ -5,24 +5,26 @@ import getRepos from '../utils/getRepos';
 const GhRepo: Component = () => {
   const [repos] = createResource(getRepos);
   return (
-    <ul class="flex flex-col gap-2 p-2 max-w-prose">
-      <For each={repos()}>
-        {(repo) => (
-          <li class="border-red-200 dark:border-red-800 border-2 border-dashed hover:border-solid rounded-md">
-            <a href={repo.html_url} target="_blank" class="flex flex-col gap-4 p-4">
-              <h6 class="underline text-red-800 dark:text-red-200">{repo.name}</h6>
-              <Show when={repo.description}>
-                <p>{repo.description}</p>
-              </Show>
-              <div class="flex gap-2 items-center">
-                <IoStar size={16} color="#e3b341" />
-                <p>{repo.stargazers_count}</p>
-              </div>
-            </a>
-          </li>
-        )}
-      </For>
-    </ul>
+    <Show when={repos()?.length !== 0}>
+      <ul class="flex flex-col gap-2 p-2 w-prose">
+        <For each={repos()}>
+          {(repo) => (
+            <li class="rounded-md shadow-md shadow-neutral-200 dark:shadow-dark-900 hover:shadow transition-all duration-200">
+              <a href={repo.html_url} target="_blank" class="flex flex-col gap-4 p-4">
+                <h6 class="underline text-red-800 dark:text-red-200">{repo.name}</h6>
+                <Show when={repo.description}>
+                  <p>{repo.description}</p>
+                </Show>
+                <div class="flex gap-2 items-center">
+                  <IoStar size={16} color="#e3b341" />
+                  <p>{repo.stargazers_count}</p>
+                </div>
+              </a>
+            </li>
+          )}
+        </For>
+      </ul>
+    </Show>
   );
 };
 
