bebd0b87b50ad9fa6336b6186e011baf35f3048a
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-07-06 12:19:11 +0800

    🐛 Global store

--- src/utils/darkMode.ts
+++ src/utils/darkMode.ts
@@ -1,16 +1,20 @@
-import { createEffect, createSignal } from 'solid-js';
+import { createEffect, createRoot, createSignal } from 'solid-js';
 
-const [dark, setDark] = createSignal(window.matchMedia('(prefers-color-scheme: dark)').matches);
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
-  setDark(e.matches);
-});
-createEffect(() => {
-  if (dark()) {
-    window.document.documentElement.classList.add('dark');
-  } else {
-    window.document.documentElement.classList.remove('dark');
-  }
-});
-const darkMode = { dark, setDark };
+const createDarkMode = () => {
+  const [dark, setDark] = createSignal(window.matchMedia('(prefers-color-scheme: dark)').matches);
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+    setDark(e.matches);
+  });
+  createEffect(() => {
+    if (dark()) {
+      window.document.documentElement.classList.add('dark');
+    } else {
+      window.document.documentElement.classList.remove('dark');
+    }
+  });
+  return { dark, setDark };
+};
+
+const darkMode = createRoot(createDarkMode);
 
 export default darkMode;
