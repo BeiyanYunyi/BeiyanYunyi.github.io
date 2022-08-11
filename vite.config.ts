61b659b8bafb6e10d209719f13127641da5be7e2
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-08-11 15:05:18 +0800

    ⬆️

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import WindiCSS from 'vite-plugin-windicss';
+import { setDefaultResultOrder } from 'node:dns';
+
+setDefaultResultOrder('verbatim');
 
 export default defineConfig({
   plugins: [solidPlugin(), WindiCSS()],
   build: {
     target: 'esnext',
   },
-  base: '',
-  server: { port: 3000 },
 });
